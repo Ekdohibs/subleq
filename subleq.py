57aede296d168d9887f84159e37a6dd3db1e904e
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2024-03-10 15:49:43 +0100

    Comments

--- subleq.py
+++ subleq.py
@@ -4,7 +4,6 @@ filename = sys.argv[1]
 f = open(filename, "rb")
 data = list(f.read())
 f.close()
-print(data)
 pc = 0
 while True:
   if pc < 0 or pc + 2 >= len(data):
@@ -13,16 +12,7 @@ while True:
   if a < 0 or a >= len(data) or b < 0 or b >= len(data):
     print("Access out of bounds:", a, b)
     break
-  #print(pc, a, b, c, data[a], data[b])
-  #if pc in [55, 100, 103, 106, 109, 112]:
-  #  print("pc =", pc, "Z = ", data[32], "U =", data[36], "V =", data[37])
-  #if pc == 100: print(data[a])
-  #  print(a, b, data[a], data[b])
-  #if a >= 124:
-  #  print("READ", data[a], "AT", a)
   data[b] -= data[a]
-  if b >= 124:
-    print("WRITE", data[b], "AT", b)
   if data[b] <= 0:
     pc = c
   else:
