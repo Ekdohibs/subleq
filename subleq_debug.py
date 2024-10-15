85d455dcd128b8be6264495eece8fed26015d469
Nathanaëlle Courant <nathanaelle.courant@ocamlpro.com>
2024-10-15 22:20:58 +0200

    Intermediate words

--- subleq_debug.py
+++ subleq_debug.py
@@ -5,12 +5,16 @@ f = open(filename, "rb")
 data = list(f.read())
 f.close()
 pc = 0
+iters = 0
 while True:
+  #if pc == 124: print("Stage 0 cycles", iters)
+  #elif pc == 581: print("Stage 1 cycles", iters)
+  iters += 1
   if pc < 0 or pc + 2 >= len(data):
     break
   a, b, c = data[pc], data[pc+1], data[pc+2]
   if a < -1 or a >= len(data) or b < -1 or b >= len(data):
-    print("Access out of bounds:", a, b)
+    print("Access out of bounds:", a, b, pc)
     break
   if a == -1:
     dataa = ord(sys.stdin.read(1))
@@ -41,3 +45,6 @@ for i in range(LABEL_LEN):
 print("Labels:", labels)
 print("Stack:", data[labels["S"]:labels["S"]+256])
 #print("Return stack:", data[labels["R"]:labels["R"]+256])
+print("Cycles:", iters)
+
+#print("".join(chr(x) if (9 <= x <= 10 or 32 <= x <= 126) else "." for x in data))
